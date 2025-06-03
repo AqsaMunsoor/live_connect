b629651902e4863e41d39bbbcceb9ecf0cfec30f
AqsaMunsoor <aqsamansur00@gmail.com>
2025-06-03 14:21:41 +0500

    unused removed

--- lib/configs/configs.dart
+++ lib/configs/configs.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:live_connect/blocs/auth/cubit.dart';
 import 'package:live_connect/models/user/user.dart';
-import 'package:live_connect/services/faults.dart';
 
 export 'package:recase/recase.dart';
 
@@ -18,6 +17,3 @@ part 'typography/typography.dart';
 part 'typography/app_text.dart';
 
 part 'color/color.dart';
-
-part 'cubit/_action.dart';
-part 'cubit/_state.dart';
